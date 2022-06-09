9c2692c2a6181eb4fb98816b12ef104288e45339
Alessandro Ferrari <alessandro.ferrari@supsi.ch>
2022-06-09 09:42:51 +0200

    Silence some OGL debug info spam

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -121,6 +121,8 @@ static std::string FormatDebugOutput(GLenum source, GLenum type, GLuint id, GLen
  */
 static void __stdcall DebugCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, GLvoid *userParam)
 {
+   if (type == GL_DEBUG_TYPE_OTHER) return;
+
    std::string error = FormatDebugOutput(source, type, id, severity, message);
    if (type == GL_DEBUG_TYPE_ERROR)
    {
